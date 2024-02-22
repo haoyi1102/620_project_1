438a4c658830247caa3794eeab60ebe48d48db7b
Liancheng <chenggg@umich.edu>
2024-02-22 18:28:39 -0500

    chenggg

--- chenggg.R
+++ chenggg.R
@@ -13,6 +13,7 @@ df <- read_excel("ScreenTime_chenggg.xlsx")
 df <- df[c(1:31), ]
 df$Pickup.1st_EST <- format(as.POSIXct(df$Pickup.1st_PST, format = "%H:%M", tz = "America/Los_Angeles"), "%H:%M", tz = "America/New_York")
 df <- df %>% select(-"Pickup.1st_PST")
+
 convert_to_minutes <- function(time) {
   if (!grepl("h", time)) {
     return(as.numeric(sub("m", "", time)))
@@ -23,6 +24,20 @@ convert_to_minutes <- function(time) {
   parts <- strsplit(time, "h|m")[[1]]
   as.numeric(parts[1]) * 60 + as.numeric(parts[2])
 }
+
+library(circular)
+time_to_degrees <- function(time) {
+  # Convert time to hours and minutes
+  hours <- as.numeric(substr(time, 1, 2))
+  minutes <- as.numeric(substr(time, 4, 5))
+  # Calculate the total hours since midnight
+  total_hours <- hours + minutes / 60
+  # Calculate the angle
+  angle <- total_hours / 24 * 360
+  return(angle)
+}
+
+Pickup.1st.angular = data.frame(sapply(df$Pickup.1st_EST, time_to_degrees))[,1]
 df$Total.ST.min <- sapply(df$Total.ST, convert_to_minutes)
 df$Social.ST.min <- sapply(df$Social.ST, convert_to_minutes)
 df$prop_ST <- df$Social.ST.min / df$Total.ST.min
@@ -31,7 +46,7 @@ df$is_weekday <- ifelse(wday(df$Date) %in% 2:6, 1, 0)
 
 ### calculate XX XY
 
-X <- as.matrix(cbind(1, df$Total.ST.min, df$Social.ST.min, df$Pickups, df$duration_per_use,df$is_weekday))
+X <- as.matrix(cbind(1, df$Total.ST.min, df$Social.ST.min, df$Pickups, df$duration_per_use,df$is_weekday,Pickup.1st.angular,df$procrastination,df$BMI, df$course_hours))
 Y <- df$prop_ST
 
 XX <- t(X) %*% X  
@@ -43,8 +58,15 @@ combined_matrix <- cbind(XX, XY = XY_matrix)
 
 combined_df <- as.data.frame(combined_matrix)
 
-write.csv(combined_df, file = "chenggg_XX_XY.csv", row.names = FALSE)
+write.csv(combined_df, "chenggg_XX_XY.csv", row.names = FALSE)
 
+### calculate YY and n
+
+YY = t(Y) %*% Y
+n = nrow(df)
+data_to_save <- data.frame(YY = c(YY[,1]), n = n)
+write.csv(data_to_save, file = "chenggg_YY_n.csv", row.names = FALSE)
+###
 result_summary <- df %>%
   group_by(is_weekday) %>%
   summarise(
@@ -81,7 +103,36 @@ result_summary <- df %>%
     q1_duration_per_use = quantile(duration_per_use, 0.25),
     q3_duration_per_use = quantile(duration_per_use, 0.75),
     min_duration_per_use = min(duration_per_use),
-    max_duration_per_use = max(duration_per_use)
+    max_duration_per_use = max(duration_per_use),
+    
+    mean_procrastination = mean(procrastination),
+    sd_procrastination = sd(procrastination),
+    q1_procrastination = quantile(procrastination, 0.25),
+    q3_procrastination = quantile(procrastination, 0.75),
+    min_procrastination = min(procrastination),
+    max_procrastination = max(procrastination),
+    
+    mean_BMI = mean(BMI),
+    sd_BMI = sd(BMI),
+    q1_BMI = quantile(BMI, 0.25),
+    q3_BMI = quantile(BMI, 0.75),
+    min_BMI = min(BMI),
+    max_BMI = max(BMI),
+    
+    mean_course_hours = mean(course_hours),
+    sd_course_hours = sd(course_hours),
+    q1_course_hours = quantile(course_hours, 0.25),
+    q3_course_hours = quantile(course_hours, 0.75),
+    min_course_hours = min(course_hours),
+    max_course_hours = max(course_hours),
+    
+    mean_Pickup.1st.angular = mean(Pickup.1st.angular),
+    sd_Pickup.1st.angular = sd(Pickup.1st.angular),
+    q1_Pickup.1st.angular = quantile(Pickup.1st.angular, 0.25),
+    q3_Pickup.1st.angular = quantile(Pickup.1st.angular, 0.75),
+    min_Pickup.1st.angular = min(Pickup.1st.angular),
+    max_Pickup.1st.angular = max(Pickup.1st.angular),
+    
   )
 
 statistical_summary <- result_summary %>%
@@ -89,8 +140,51 @@ statistical_summary <- result_summary %>%
   separate(name, into = c("variable", "stat"), sep = "_") %>%
   pivot_wider(names_from = stat, values_from = value)
 
-write.csv(statistical_summary, file = "SummaryStatchenggg.csv", row.names = TRUE)
 
+###
+write.csv(statistical_summary, file = "SummaryStatchenggg.csv", row.names = TRUE)
+### plot
+
+statistical_summary <- statistical_summary[c(1,7),]
+
+library(gt)
+library(dplyr)
+
+statistical_summary <- statistical_summary %>%
+  mutate(
+    is_weekday = case_when(
+      is_weekday == 0 ~ "Weekends",
+      is_weekday == 1 ~ "Weekday"
+    )
+  )
+
+visual_table_1 <- statistical_summary %>%
+  select(-variable) %>%
+  gt() %>%
+  tab_header(
+    title = "Descriptive Statistics by Weekday",
+    subtitle = "Presenting Mean Values"
+  ) %>%
+  cols_label(
+    is_weekday = "Is Weekday",
+    Total = "Total",
+    Social = "Social",
+    Pickups = "Pickups",
+    prop = "Proportion",
+    duration = "Duration",
+    procrastination = "Procrastination",
+    BMI = "BMI",
+    course = "Course",
+    Pickup.1st.angular = "Pickup.1st.Angular"
+  ) %>%
+  fmt_number(
+    columns = vars(Total, Social, Pickups, prop, duration, procrastination, BMI, course, Pickup.1st.angular),
+    decimals = 2
+  )
+
+gtsave(visual_table_1, filename = "./Figure/statistical_summary_chenggg.html")
+
+### boxplot
 df$Date <- as.Date(df$Date)
 df$is_weekday <- factor(df$is_weekday)
 proportion.plot = ggplot(df, aes(x = Date, y = prop_ST, 
@@ -116,4 +210,5 @@ boxplot_plot <- ggplot(df, aes(x = is_weekday, y = prop_ST, fill = is_weekday)) 
   theme_minimal()
 
 ggsave("./Figure/box_plot_chenggg.png", plot = proportion.plot, width = 10, height = 6, units = "in")
+
 
